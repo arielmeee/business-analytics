26a4352caa7c40c73494f4bdedcc5d6f4681a5ef
Ariel Llanita <llanitaariel@gmail.com>
2021-12-08 19:32:01 +0800

    Add dummy data

---
+++ src/dummy_data/promotions.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    type: "Category One",
+    value: 27,
+  },
+  {
+    type: "Category Two",
+    value: 25,
+  },
+  {
+    type: "Category Three",
+    value: 18,
+  },
+  {
+    type: "Category Four",
+    value: 15,
+  },
+  {
+    type: "Category Five",
+    value: 10,
+  },
+  {
+    type: "Other",
+    value: 5,
+  },
+];
+
+export default data
---
+++ src/dummy_data/sales.js
@@ -0,0 +1,84 @@
+const data = [
+  {
+    name: "RRJ",
+    month: "Jan.",
+    revenue: 18.9,
+  },
+  {
+    name: "RRJ",
+    month: "Feb.",
+    revenue: 28.8,
+  },
+  {
+    name: "RRJ",
+    month: "Mar.",
+    revenue: 39.3,
+  },
+  {
+    name: "RRJ",
+    month: "Apr.",
+    revenue: 81.4,
+  },
+  {
+    name: "RRJ",
+    month: "May",
+    revenue: 47,
+  },
+  {
+    name: "RRJ",
+    month: "Jun.",
+    revenue: 20.3,
+  },
+  {
+    name: "RRJ",
+    month: "Jul.",
+    revenue: 24,
+  },
+  {
+    name: "RRJ",
+    month: "Aug.",
+    revenue: 35.6,
+  },
+  {
+    name: "Nike",
+    month: "Jan.",
+    revenue: 12.4,
+  },
+  {
+    name: "Nike",
+    month: "Feb.",
+    revenue: 23.2,
+  },
+  {
+    name: "Nike",
+    month: "Mar.",
+    revenue: 34.5,
+  },
+  {
+    name: "Nike",
+    month: "Apr.",
+    revenue: 99.7,
+  },
+  {
+    name: "Nike",
+    month: "May",
+    revenue: 52.6,
+  },
+  {
+    name: "Nike",
+    month: "Jun.",
+    revenue: 35.5,
+  },
+  {
+    name: "Nike",
+    month: "Jul.",
+    revenue: 37.4,
+  },
+  {
+    name: "Nike",
+    month: "Aug.",
+    revenue: 42.4,
+  },
+]
+
+export default data
